83ddefad87aea0fe15f627f77b61c70f99c4a79c
marcos.damasceno <marcos.damasceno@masterline.ind.br>
2022-08-25 13:33:39 -0300

    Feat: criação das classes de produto

--- src/main/java/br/com/force/sale/produto/Produto.java
+++ src/main/java/br/com/force/sale/produto/Produto.java
@@ -1,5 +1,16 @@
 package br.com.force.sale.produto;
 
+import java.math.BigDecimal;
+
 public class Produto {
+
+    private String codProduto;
+    private String codOrigem;
+    private String codFamilia;
+    private String codDerivacao;
+    private String DescricaoProduto;
+    private String nomeProduto;
+    private String situacaoProduto;
+    private Boolean prodVenda;
 
 }
